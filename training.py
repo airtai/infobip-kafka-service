1df6ba0baaac082f29995d0927d0d2b3fd89f7e1
Kumaran Rajendhiran <kumaran@airt.ai>
2023-07-11 15:35:25 +0530

    Add init python files

---
+++ training.py
@@ -0,0 +1,12 @@
+from os import environ
+from pathlib import Path
+
+from airt.infobip.kafka_training import create_app
+
+
+root_path = Path(environ["ROOT_PATH"])
+downloading_group_id = environ["DOWNLOADING_GROUP_ID"]
+training_group_id = environ["TRAINING_GROUP_ID"]
+
+
+app = create_app(root_path=root_path, downloading_group_id=downloading_group_id, training_group_id=training_group_id)
