b8f4b631d7ef691e8b0120399056079af19cd260
Kumaran Rajendhiran <kumaran@airt.ai>
2023-07-12 17:20:53 +0530

    Start dummy command in docker container

--- scripts/start_webservice.sh
+++ scripts/start_webservice.sh
@@ -17,8 +17,9 @@ echo NUM_WORKERS set to $NUM_WORKERS
 KAFKA_BROKER="staging"
 echo KAFKA_BROKER value set to $KAFKA_BROKER
 
-fastkafka run --num-workers $NUM_WORKERS --kafka-broker $KAFKA_BROKER downloading:app > ./downloading.log & 
+# fastkafka run --num-workers $NUM_WORKERS --kafka-broker $KAFKA_BROKER downloading:app > ./downloading.log & 
 
-fastkafka run --num-workers $NUM_WORKERS --kafka-broker $KAFKA_BROKER training:app > ./training.log & 
+# fastkafka run --num-workers $NUM_WORKERS --kafka-broker $KAFKA_BROKER training:app > ./training.log & 
 
-tail -f training.log
+# tail -f training.log
+watch -n 5 free -m
