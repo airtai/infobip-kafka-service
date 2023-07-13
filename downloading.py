df488f8348250d961908281c3c54219108f3f6e3
Kumaran Rajendhiran <kumaran@airt.ai>
2023-07-13 17:38:41 +0530

    Add downloading_group_id to downloading root_path

--- downloading.py
+++ downloading.py
@@ -9,4 +9,4 @@ downloading_group_id = environ["DOWNLOADING_GROUP_ID"]
 # training_group_id = environ["TRAINING_GROUP_ID"]
 
 
-app = create_app(root_path=root_path, group_id=downloading_group_id)
+app = create_app(root_path=root_path/downloading_group_id, group_id=downloading_group_id)
