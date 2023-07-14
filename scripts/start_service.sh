ea7af39138a4e8745f2f8bc8800c52c649f12722
Kumaran Rajendhiran <kumaran@airt.ai>
2023-07-14 10:09:40 +0530

    Invert commands in start script as experiment

--- scripts/start_service.sh
+++ scripts/start_service.sh
@@ -10,6 +10,9 @@ echo NUM_WORKERS set to $NUM_WORKERS
 KAFKA_BROKER="staging"
 echo KAFKA_BROKER value set to $KAFKA_BROKER
 
-fastkafka run --num-workers $NUM_WORKERS --kafka-broker $KAFKA_BROKER downloading:app > ./downloading.log & 
+# fastkafka run --num-workers $NUM_WORKERS --kafka-broker $KAFKA_BROKER downloading:app > ./downloading.log & 
 
-fastkafka run --num-workers $NUM_WORKERS --kafka-broker $KAFKA_BROKER training:app
+fastkafka run --num-workers $NUM_WORKERS --kafka-broker $KAFKA_BROKER training:app > ./training.log & 
+
+
+fastkafka run --num-workers $NUM_WORKERS --kafka-broker $KAFKA_BROKER downloading:app
