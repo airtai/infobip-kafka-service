db1b6d18c79146d0a7be3a32219e0a9c703dba46
Kumaran Rajendhiran <kumaran@airt.ai>
2023-07-14 10:38:44 +0530

    Revert start service script commands

--- scripts/start_service.sh
+++ scripts/start_service.sh
@@ -10,9 +10,6 @@ echo NUM_WORKERS set to $NUM_WORKERS
 KAFKA_BROKER="staging"
 echo KAFKA_BROKER value set to $KAFKA_BROKER
 
-# fastkafka run --num-workers $NUM_WORKERS --kafka-broker $KAFKA_BROKER downloading:app > ./downloading.log & 
+fastkafka run --num-workers $NUM_WORKERS --kafka-broker $KAFKA_BROKER downloading:app > ./downloading.log & 
 
-fastkafka run --num-workers $NUM_WORKERS --kafka-broker $KAFKA_BROKER training:app > ./training.log & 
-
-
-fastkafka run --num-workers $NUM_WORKERS --kafka-broker $KAFKA_BROKER downloading:app
+fastkafka run --num-workers $NUM_WORKERS --kafka-broker $KAFKA_BROKER training:app
