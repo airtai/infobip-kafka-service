c28da9502b4eb7a6e9226d33e9d2cfd3a33e5e09
Kumaran Rajendhiran <kumaran@airt.ai>
2023-07-20 13:42:55 +0530

    Format py files

--- training.py
+++ training.py
@@ -9,4 +9,8 @@ downloading_group_id = environ["DOWNLOADING_GROUP_ID"]
 training_group_id = environ["TRAINING_GROUP_ID"]
 
 
-app = create_app(root_path=root_path, downloading_group_id=downloading_group_id, training_group_id=training_group_id)
+app = create_app(
+    root_path=root_path,
+    downloading_group_id=downloading_group_id,
+    training_group_id=training_group_id,
+)
