b11c5a8098e6ba273d6af5068240bb6d40b137e3
Kumaran Rajendhiran <kumaran@airt.ai>
2023-08-10 13:53:26 +0530

    Fix multi command bugs

--- scripts/ci_check_certs.sh
+++ scripts/ci_check_certs.sh
@@ -5,7 +5,8 @@ THRESHOLD=365 # ToDo: Change this to 5
 
 
 # Check if certificate is expiring within the threshold
-expiration_date=$(date -d "$(ssh -o StrictHostKeyChecking=no -i key.pem azureuser@$KAFKA_DOMAIN 'sudo openssl x509 -in /etc/letsencrypt/live/$KAFKA_DOMAIN/fullchain.pem -noout -enddate | cut -d= -f2')" +%s)
+expiry_str=$(ssh -o StrictHostKeyChecking=no -i key.pem azureuser@"$KAFKA_DOMAIN" "sudo openssl x509 -in /etc/letsencrypt/live/$KAFKA_DOMAIN/fullchain.pem -noout -enddate | cut -d= -f2")
+expiration_date=$(date -d "$expiry_str" +%s)
 current_date=$(date +%s)
 days_until_expiry=$(( (expiration_date - current_date) / 86400 ))
 
